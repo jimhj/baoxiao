7a31232efdc2f8df6603ca1776afd848df2b34c5
jimmy.huangjin <jimmy.huangjin@gmail.com>
2014-06-09 12:03:46 +0800

    modify pagination for mobile devices

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,5 @@
 class IndexController < ApplicationController
   def index
-    @jokes = Joke.paginate(:page => params[:page], :per_page => 20)
+    @jokes = Joke.paginate(:page => params[:page], :per_page => 20, total_entries: 3000)
   end
 end
--- lib/parser/qiubai.rb
+++ lib/parser/qiubai.rb
@@ -8,7 +8,7 @@ module Parser
     def initialize(opts = {})
       @opts = opts 
       @opts[:page_start] ||= 1
-      @opts[:page_stop] ||= 10
+      @opts[:page_stop] ||= 1
       @user = User.first
     end
 
@@ -17,18 +17,24 @@ module Parser
         Rails.logger.info('*' * 60)
         Rails.logger.info("开始抓取第 #{page} 页")
 
-        url = "#{SCRAP_FROM}/page/#{@opts[:page_start]}"
+        url = "#{SCRAP_FROM}/page/#{page}"
         uri = URI url
         parser = Nokogiri::HTML(uri.open({ "User-Agengt" => USER_AGENT }))
 
         parser.xpath("//div[@class='article block untagged mb15']").each do |c|
           content = c.xpath("./div[@class='content']").first.try(:content)
           picture = c.xpath("./div[@class='thumb']//img[1]/@src").first.try(:value)
+          up_votes = c.xpath(".//li[@class='up']/a[@class='voting']/span[1]").first.try(:content)
+          down_votes = c.xpath(".//li[@class='down']/a[@class='voting']/span[1]").first.try(:content)
 
           joke = @user.jokes.build
           joke.anonymous = true
           joke.content = content
           joke.remote_picture_url = picture if picture
+          joke.up_votes_count = up_votes.to_i
+          joke.down_votes_count = down_votes.to_i
+          joke.from = '糗事百科'
+
           unless joke.save
             Rails.logger.info("#{content} 保存失败")
             Rails.logger.info(joke.errors.full_messages)
