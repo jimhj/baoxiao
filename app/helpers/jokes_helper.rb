8b2e81e88a6a1fbb3d98057255840212a51d3e17
Jimmy Huang <jimmy.huangjin@gmail.com>
2014-06-21 13:43:48 +0800

    update tag cloud view

--- app/helpers/jokes_helper.rb
+++ app/helpers/jokes_helper.rb
@@ -5,4 +5,10 @@ module JokesHelper
     return if joke.nil?
     joke_path joke
   end
+
+  def render_tag tag
+    size = (12..24).to_a.sample
+    color = "%06x" % (rand * 0xffffff)
+    link_to tag.name, tag_path(tag.name), :class => "tag", :style => "font-size: #{size}px; color: ##{color};"
+  end
 end
