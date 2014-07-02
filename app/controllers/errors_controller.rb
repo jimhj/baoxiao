b9b986d3224992123b0ca24766fcf0cdf4f23026
jimmy.huangjin <jimmy.huangjin@gmail.com>
2014-07-02 14:22:56 +0800

    404 to 302

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,15 +1,6 @@
 class ErrorsController < ApplicationController
   def not_found
     @joke = Joke.random || Joke.order('hot DESC').first
-    set_meta_data
-    render template: 'jokes/show'    
-  end
-  
-  def set_meta_data
-    title = @joke.title.presence || @joke.content
-    tag_list = @joke.tags.map(&:name)
-    keywords = ([title] + tag_list).join(',')
-    description = ([title, @joke.content] + tag_list).join(',')
-    set_seo_meta title, keywords, description    
-  end  
+    redirect_to joke_path(@joke), status: 302    
+  end 
 end
