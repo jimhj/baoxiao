78dcdf29595945802e311dc828f1da268820203f
Jimmy Huang <jimmy.huangjin@gmail.com>
2014-08-14 22:39:18 +0800

    update tag show page meat

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -11,7 +11,7 @@ class TagsController < ApplicationController
     @jokes = Joke.tagged_with(params[:id]).order('id DESC').paginate(page: params[:page], per_page: 20, total_entries: 2000)
 
     page_title = "#{t('tags.meta.title', tag: params[:id])},#{Settings.app_title}"
-    description = "#{t('tags.meta.description', tag: params[:id])}"
+    description = "#{t('tags.meta.description', tag: @tag.description)}"
     set_seo_meta page_title, params[:id], description
   end
 end
