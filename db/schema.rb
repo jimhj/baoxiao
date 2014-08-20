34b392395e872c75041726e99d6d9c395f068a04
Jimmy Huang <jimmy.huangjin@gmail.com>
2014-08-20 21:26:01 +0800

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 20140820093754) do
   create_table "friend_sites", force: true do |t|
     t.string   "name"
     t.string   "url"
-    t.integer  "priority",   default: 1000, null: false
+    t.integer  "priority",   default: 1000
     t.integer  "status",     default: 0
     t.integer  "user_id"
     t.datetime "created_at"
