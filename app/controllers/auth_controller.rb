7bab02f14703d8a70d932b56d2d31aea06ff5791
jimmy.huangjin <jimmy.huangjin@gmail.com>
2014-08-22 14:25:38 +0800

    weibo auth done

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,15 +3,14 @@ class AuthController < ApplicationController
 
   def weibo_callback
     auth = request.env["omniauth.auth"]
-
+    Rails.logger.info auth
     user = User.find_or_create_by!(weibo_uid: auth.uid.to_s) do |u|
       if u.new_record?
         u.email = "#{SecureRandom.hex(6)}@weibo.random.com"
         u.remote_avatar_url = File.join(auth.info.avatar_url, 'avatar.jpg')
         u.password = SecureRandom.hex(8)
+        u.name = auth.info.name
       end
-
-      u.name = auth.info.name
       u.weibo_token = auth.credentials.token
     end
 
